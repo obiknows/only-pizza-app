30c0ec4216eeae57a1710aa0adc6c72ad29816ac
Obinna <obiknows88@gmail.com>
2023-11-21 12:00:08 -0500

    removed link

--- app/page.tsx
+++ app/page.tsx
@@ -64,7 +64,6 @@ export default async function Home() {
 			>
 				<img src={product.avatarUrl} alt={product.title} />
 				<h2 className="italic">{product.title}</h2>
-				{/* <p>{product.description}</p> */}
 				<p>${product.price}</p>
 			</div>
 		);
@@ -88,14 +87,9 @@ export default async function Home() {
 					🍕 Only Pizza
 				</p>
 				<div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-					<a
-						className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-						href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-						target="_blank"
-						rel="noopener noreferrer"
-					>
+					<p className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0">
 						By OBINNA
-					</a>
+					</p>
 				</div>
 			</div>
 
